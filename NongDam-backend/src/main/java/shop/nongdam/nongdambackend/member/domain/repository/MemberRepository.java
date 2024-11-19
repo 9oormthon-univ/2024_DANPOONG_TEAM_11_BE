10ac89383766fb655298e0886c362d119efd3e3e
dongkyunKim <kyun010713@gmail.com>
2024-11-19 18:21:18 +0900

    fix: 이메일 반환하는 어노테이션 오류 해결

--- NongDam-backend/src/main/java/shop/nongdam/nongdambackend/member/domain/repository/MemberRepository.java
+++ NongDam-backend/src/main/java/shop/nongdam/nongdambackend/member/domain/repository/MemberRepository.java
@@ -2,8 +2,9 @@ package shop.nongdam.nongdambackend.member.domain.repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import shop.nongdam.nongdambackend.member.domain.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, JpaSpecificationExecutor<Member> {
     Optional<Member> findByEmail(String email);
 }
