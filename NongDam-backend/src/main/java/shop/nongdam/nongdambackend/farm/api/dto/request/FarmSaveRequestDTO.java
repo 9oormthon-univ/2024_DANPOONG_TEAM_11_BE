8aa86c2e4c715fd1cbace00648895e0a5c1fc690
wonjun0120 <wonjundero@gmail.com>
2024-11-24 04:23:12 +0900

    feat: farm img 삭제 (#43)

--- NongDam-backend/src/main/java/shop/nongdam/nongdambackend/farm/api/dto/request/FarmSaveRequestDTO.java
+++ NongDam-backend/src/main/java/shop/nongdam/nongdambackend/farm/api/dto/request/FarmSaveRequestDTO.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotBlank;
 public record FarmSaveRequestDTO(
         @NotBlank(message = "농가 이름은 필수 입력값입니다.")
         String farmName,
-        String profileImage,
         @NotBlank(message = "농가 대표자는 필수 입력값입니다.")
         String farmRepresentative,
         @NotBlank(message = "농가 전화번호는 필수 입력값입니다.")
